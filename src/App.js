c134196f54649fc4501b9bbb37546c589fc5f39c
laimac <55thmouse@gmail.com>
2022-09-07 15:12:51 -0700

    Added bootstrap columns

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
 import Weather from "./Weather";
+import './App.css';
 
 function App() {
   return (
@@ -8,7 +7,7 @@ function App() {
 <div className="container">
 <Weather />
 <footer>
-This project was coded by Laima Ciselonoka and is {" "} <a href="https://github.com/" target="_blank"> open-sourced on GitHub</a>
+This project was coded by Laima Ciselonoka and is {" "} <a href="https://github.com/" target="_blank" rel="noreferrer"> open-sourced on GitHub</a>
 </footer>
 </div>
     </div>
--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,19 @@
 import React from "react";
-import "./Weather.css";
+
 
 export default function Weather(){
     return(
         <div className="Weather">
+            <form>
+                <div className="row">
+                    <div className="col-9">
+                <input type="search" placeholder="Enter a city.." className="form-control"/>
+                </div>
+                <div className="col-3">
+                <input type="submit" value="Search" className="btn btn-primary"/>
+                </div>
+                </div>
+            </form>
             <h1>
                 Jokkmokk
             </h1>

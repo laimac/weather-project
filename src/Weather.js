4171c412b1ef5e848336855bf670a45ad0f6ffb5
laimac <55thmouse@gmail.com>
2022-09-07 14:57:54 -0700

    Added html code

--- src/Weather.js
+++ src/Weather.js
@@ -4,7 +4,35 @@ import "./Weather.css";
 export default function Weather(){
     return(
         <div className="Weather">
-            Hello
+            <h1>
+                Jokkmokk
+            </h1>
+            <ul>
+                <li>
+                    Wednesday 07:00
+                </li>
+                <li>
+                    Mostly cloudy
+                </li>
+            </ul>
+            <div className="row">
+                <div className="col-6">
+                    <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Mostly cloudy" />
+               6 C
+                </div>
+                <div className="col-6">
+                    <ul>
+                        <li>
+                            Precipitation: 50%
+                        </li>
+                        <li>
+                            Humidity: 72%
+                        </li>                        <li>
+                            Wind: 8 m/s
+                        </li>
+                    </ul>
+                </div>
+            </div>
             </div>
     )
 }
